d8ea97ed99b3b0723ebe59c0a24be11a4c516e68
Vinicius Hespanha <mail@vhespanha.dev>
2025-11-05 20:03:47 -0300

    add click handling
    add logic to define and manipulate clickable objects

--- game.go
+++ game.go
@@ -5,31 +5,65 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Game struct{}
+var (
+	Blue  = color.RGBA{0, 0, 0xff, 0xff}
+	Red   = color.RGBA{0xff, 0, 0, 0xff}
+	Green = color.RGBA{0, 0xff, 0, 0xff}
+)
+
+type Rectangle struct {
+	size, x, y int
+	col        color.RGBA
+}
+
+func (r *Rectangle) IsClicked(cx, cy int) bool {
+	return cx >= r.x && cx < r.x+r.size && cy >= r.y && cy < r.y+r.size
+}
+
+func (r *Rectangle) SwapColor() {
+	if r.col == Green {
+		r.col = Blue
+	} else {
+		r.col = Green
+	}
+}
+
+type Game struct {
+	rect *Rectangle
+}
 
 func newGame() *Game {
-	return &Game{}
+	rx, ry := Center.Position(100, 100)
+	rectangle := &Rectangle{
+		size: 100,
+		x:    int(rx),
+		y:    int(ry),
+		col:  Red,
+	}
+	return &Game{rectangle}
 }
 
 func (g *Game) Update() error {
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		cx, cy := ebiten.CursorPosition()
+		if g.rect.IsClicked(cx, cy) {
+			g.rect.SwapColor()
+		}
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	blue := color.RGBA{0, 0, 0xff, 0xff}
-	red := color.RGBA{0xff, 0, 0, 0xff}
-
-	var w, h float32
-	w, h = 100.0, 100.0
-	x, y := Center.Position(w, h)
-	vector.FillRect(screen, x, y, w, h, blue, false)
+	vector.FillRect(screen, float32(g.rect.x), float32(g.rect.y), float32(g.rect.size), float32(g.rect.size), g.rect.col, false)
 
 	cx, cy := ebiten.CursorPosition()
 	var r float32
 	r = 20
-	vector.FillCircle(screen, float32(cx), float32(cy), r, red, false)
+	vector.FillCircle(screen, float32(cx), float32(cy), r, Red, false)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
