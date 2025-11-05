8d073b8958e9c1e167c62f4ad3f775adacc20597
Vinicius Hespanha <mail@vhespanha.dev>
2025-11-05 18:53:46 -0300

    add cursor tracking
    start tracking cursor and add an indicator to where it's positioned

--- game.go
+++ game.go
@@ -19,10 +19,17 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	blue := color.RGBA{0, 0, 0xff, 0xff}
+	red := color.RGBA{0xff, 0, 0, 0xff}
+
 	var w, h float32
 	w, h = 100.0, 100.0
 	x, y := Center.Position(w, h)
 	vector.FillRect(screen, x, y, w, h, blue, false)
+
+	cx, cy := ebiten.CursorPosition()
+	var r float32
+	r = 20
+	vector.FillCircle(screen, float32(cx), float32(cy), r, red, false)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
