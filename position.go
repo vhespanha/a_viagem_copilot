53ae1dc7ff49708af066a767a87987cb5038b4e6
Vinicius Hespanha <mail@vhespanha.dev>
2025-11-06 16:00:02 -0300

    update position to use offsets

--- position.go
+++ position.go
@@ -1,23 +1,28 @@
 package main
 
 type Anchor struct {
-	Ax, Ay float32
+	AnchorX, AnchorY float32
+	OffsetX, OffsetY float32
 }
 
 func (a Anchor) Position(objW, objH float32) (float32, float32) {
-	x := (ScreenWidth - objW) * a.Ax
-	y := (ScreenHeight - objH) * a.Ay
+	x := (ScreenWidth-objW)*a.AnchorX + a.OffsetX
+	y := (ScreenHeight-objH)*a.AnchorY + a.OffsetY
 	return x, y
 }
 
+func (a Anchor) Offset(x, y float32) Anchor {
+	return Anchor{a.AnchorX, a.AnchorY, a.OffsetX + x, a.OffsetY + y}
+}
+
 var (
-	TopLeft      = Anchor{0, 0}
-	TopCenter    = Anchor{0.5, 0}
-	TopRight     = Anchor{1, 0}
-	CenterLeft   = Anchor{0, 0.5}
-	Center       = Anchor{0.5, 0.5}
-	CenterRight  = Anchor{1, 0.5}
-	BottomLeft   = Anchor{0, 1}
-	BottomCenter = Anchor{0.5, 1}
-	BottomRight  = Anchor{1, 1}
+	TopLeft      = Anchor{0, 0, 0, 0}
+	TopCenter    = Anchor{0.5, 0, 0, 0}
+	TopRight     = Anchor{1, 0, 0, 0}
+	CenterLeft   = Anchor{0, 0.5, 0, 0}
+	Center       = Anchor{0.5, 0.5, 0, 0}
+	CenterRight  = Anchor{1, 0.5, 0, 0}
+	BottomLeft   = Anchor{0, 1, 0, 0}
+	BottomCenter = Anchor{0.5, 1, 0, 0}
+	BottomRight  = Anchor{1, 1, 0, 0}
 )
